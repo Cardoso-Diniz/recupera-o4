768a428dddca442939a72ccce70e219bbc941bad
Leonardo Souza <leonardo030605@gmail.com>
2021-07-21 10:57:37 -0300

    MVC inicio

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using recuperação4.Models;
+
+namespace InstaDev.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+
+    }
+}
